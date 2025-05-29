4f0a94be04c931faa4b22c41bf598f5b6d3a41dd
kosa <kosa@kosa.com>
2025-05-29 10:24:18 +0900

    main_views.py에 있는 질문 관련 URL 기능을 question_views.py 파일로 분리하여 사용 하는 기능 추가, / url 실행시 question.list로 redirect 하는 기능 추가됨

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template
-from pybo.models import  Question
+from flask import Blueprint, url_for, redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -7,13 +6,6 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def hello_pybo():
     return 'Hello, Pybo!'
 
-@bp.route('/detail/<int:question_id>')
-def detail(question_id):
-    question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question = question )
-
-
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list = question_list)
+    return redirect( url_for('question._list'))
